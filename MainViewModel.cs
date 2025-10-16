c6cd610a4769783b5059d10399eb0c8e49208ce2
吳塏鈞 <4b3g0048@office.stust.edu.tw>
2025-10-16 14:59:32 +0800

    移除多餘的結尾標籤
    移除了 `<DockPanel>` 和 `<StatusBar>` 元素的結尾標籤，
    以清理多餘的代碼並提升代碼可讀性。
    
    更新 XAML 命名空間與佈局，新增 NuGet 套件引用
    
    在 `MainWindow.xaml` 中：
    - 移除 `xmlns:av`，新增 `xmlns:b` 和 `xmlns:xctk`。
    - 更新 `mc:Ignorable` 的值為 `b`。
    - 調整 `x:Class` 的位置與屬性順序。
    - 簡化佈局，移除 `<DockPanel>` 和 `<StatusBar>` 結構。
    
    在 `WpfApp3.csproj` 中：
    - 新增 NuGet 套件引用：`Extended.Wpf.Toolkit`，版本 `5.0.0`。

---
+++ MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp3
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!(object.Equals(field, newValue)))
+            {
+                field = (newValue);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
